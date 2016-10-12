a89a24978d07a1e9199897bd987b580d11cce19a
shekit <as7790@nyu.edu>
2016-10-12 01:46:59 -0400

    changed first verb to question

--- public/js/global.js
+++ public/js/global.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 
 	$(".verb").typed({
 		//place space before strings otherwise doesnt render with html tags properly
-		strings: [' <span class="imagine">imagine</span>',' <span class="make">make</span>',' <span class="code">code</span>',' <span class="design">design</span>',' <span class="invent">invent</span>'],
+		strings: [' <span class="question">question</span>',' <span class="make">make</span>',' <span class="code">code</span>',' <span class="design">design</span>',' <span class="invent">invent</span>'],
 		typeSpeed: 80,
 		backSpeed: 30,
 		backDelay: 1500,
