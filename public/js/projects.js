033c7521d03d0c8189ff2fdd96dd92b99e87db37
shekit <as7790@nyu.edu>
2016-10-16 01:15:24 -0400

    added gifs, creating json, added some full size images

--- public/js/projects.js
+++ public/js/projects.js
@@ -4,9 +4,16 @@ window.projects = [
 		"slug":"balsacars",
 		"description":"Balsa cars are scale models lovingly and painstakingly handcrafted by me from balsa wood. Each model takes approximately 100 hours to complete.",
 		"thumbnail":"./public/img/thumbnails/balsacars.gif",
-		"media":[{"path":"","caption":"","type":"image"}], 
+		"media":[
+			{"path":"./public/img/projects/balsacars/1.jpg","caption":"1935 Buick 90L","type":"image"},
+			{"path":"./public/img/projects/balsacars/2.jpg","caption":"1959 Cadillac Eldorado","type":"image"},
+			{"path":"./public/img/projects/balsacars/3.jpg","caption":"1959 Cadillac & 1930 Stutz","type":"image"},
+			{"path":"./public/img/projects/balsacars/4.jpg","caption":"1925 Rolls Royce","type":"image"},
+			{"path":"./public/img/projects/balsacars/5.jpg","caption":"1935 Buick 90L","type":"image"}
+			], 
 		"category":["play"],
 		"tags":["balsa","wood","handcrafted","cars"],
+		"builtWith":["balsa wood","hand"],
 		"role":["everything"],
 		"press":[],
 		"link":{"display":"balsacars.com","link":"http://balsacars.com","status":"active"}
@@ -14,15 +21,247 @@ window.projects = [
 	{
 		"title":"Grafficars",
 		"slug":"grafficars",
-		"description":"A unique initiative I started to support local artisans combines model cars with village arts to create exquisite three dimensional works of arts.",
-		"thumbnail":"./public/img/thumbnails/balsacars.gif",
-		"media":[{"path":"","caption":"","type":"image/video"}], 
+		"description":"A unique initiative I started to support local artisans combines model cars with Indian village arts to create exquisite three dimensional works of arts.",
+		"thumbnail":"./public/img/thumbnails/grafficars.gif",
+		"media":[
+			{"path":"./public/img/projects/grafficars/1.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/2.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/3.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/4.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/5.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/6.jpg","caption":"Kundan Bandhi art","type":"image"},
+			{"path":"./public/img/projects/grafficars/7.jpg","caption":"Miniature Painting","type":"image"},
+			{"path":"./public/img/projects/grafficars/8.jpg","caption":"Miniature Painting","type":"image"},
+			{"path":"./public/img/projects/grafficars/9.jpg","caption":"Mehndi","type":"image"},
+			{"path":"./public/img/projects/grafficars/10.jpg","caption":"Mehndi","type":"image"}
+			], 
 		"category":["play"],
 		"tags":["art","cars"],
+		"builtWith":["plaster","hand"],
 		"role":["concept","marketing","outreach","fabrication"],
 		"press":[],
 		"link":{"display":"grafficars.com","link":"http://grafficars.com","status":"inactive"}
-	}
+	},
+	{
+		"title":"Brand Identities",
+		"slug":"brand-identities",
+		"description":"Various brand identities designed for different companies, projects and startups.",
+		"thumbnail":"./public/img/thumbnails/brand-identities.gif",
+		"media":[
+			{"path":"./public/img/projects/brand-identities/1.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/2.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/3.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/4.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/5.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/6.jpg","caption":"","type":"image"},
+			{"path":"./public/img/projects/brand-identities/7.jpg","caption":"","type":"image"}
+			], 
+		"category":["design"],
+		"tags":["illustrator","logo"], // not used yet
+		"builtWith":["illustrator","photoshop"],
+		"role":["concept","design"],
+		"press":[],
+		"link":{}
+	},
+	{
+		"title":"Videos",
+		"slug":"videos",
+		"description":"",
+		"thumbnail":"./public/img/thumbnails/videos.gif",
+		"media":[
+			{"path":"https://www.youtube.com/embed/EpxMRmhFxDI","caption":"Imly.in - the online homefood marketplace","type":"video"},
+			{"path":"https://www.youtube.com/embed/byH-dbdq-xo","caption":"Unofficial ITP Commercial","type":"video"},
+			{"path":"http://www.youtube.com/embed/HQBNb323HSs","caption":"iVolunteer Awards for Social Change","type":"video"},
+			{"path":"https://www.youtube.com/embed/_aVR7_lZxv4","caption":"Mother Teresa Awards for Social Justice","type":"video"},
+			], 
+		"category":["design"],
+		"tags":["adobe premiere","after effects"],
+		"builtWith":["adobe premiere","adobe after effects", "adobe photoshop"],
+		"role":["design","direction","editing"],
+		"press":[],
+		"link":{}
+	},
+	{
+		"title":"Flappy Man",
+		"slug":"flappy-man",
+		"description":"Iron Man meets Flappy Bird in this game with a twist. Players wear a custom made Iron Man glove laden with sensors and flap their arms to control the trajectory and prevent hitting the obstacles",
+		"thumbnail":"./public/img/thumbnails/flappy-man.gif",
+		"media":[{"path":"","caption":"","type":"video"}],
+		"category":["play","code"],
+		"tags":["processing","arduino"],
+		"builtWith":["processing","arduino"],
+		"role":["design","concept","code","sensors"],
+		"press":[],
+		"link":{}
+	},
+	{
+		"title":"Font Fling",
+		"slug":"fontfling",
+		"description":"Finding new fonts online and trying them is hard! So I made fontfling - to bring that same feeling of quickly cycling through fonts like in Illustrator to the web. The user can then download the font(s) they like",
+		"thumbnail":"./public/img/thumbnails/fontfling.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code","design"],
+		"tags":["javascript","meteor"], // not used yet
+		"builtWith":["meteor.js","javascript","python"],
+		"role":["full stack","design","concept",],
+		"press":[
+			{"publication":"NBC Daily","link":"http://www.nbcdaily.com/5-sites-to-discover-and-download-free-fonts/3349/"},
+			{"publication":"Make Use Of","link":"http://www.makeuseof.com/tag/5-sites-discover-download-free-fonts/"},
+			{"publication":"Creagratis","link":"http://www.creagratis.com/siti-trovare-scaricare-font-gratis/"},
+			{"publication":"Creativos Online","link":"http://www.creativosonline.org/blog/font-fling-herramienta-web-perfecta-para-encontrar-fuentes-open-source-de-gran-calidad.html"},
+			{"publication":"Soft and Apps","link":"http://www.softandapps.info/2015/10/01/font-fling-previsualiza-y-descarga-fuentes-de-texto-gratuitas/"},
+			{"publication":"Technet","link":"http://technet.idnes.cz/font-zdarma-cxa-/sw_internet.aspx?c=A151103_193137_software_dvr"},
+			{"publication":"Sosyal Medya","link":"http://sosyalmedya.co/ucretsiz-font-siteleri/"},
+			{"publication":"Parapnte","link":"http://parapnte.educacion.navarra.es/2015/09/24/formas-de-encontrar-una-fuente-de-texto-concreta/"},
+			],
+		"link":{"display":"FontFling.com","link":"http://fontfling.com","status":"active"}
+	},
+	{
+		"title":"Ceven",
+		"slug":"ceven",
+		"description":"Designed for efficiency, Ceven is a digest of the day’s seven most important stories summarized to be read in under 60 words. Under the hood, the app measures click-throughs, shares, usage duration, and feedback given by users on each daily digest which are used to personalize the digests.",
+		"thumbnail":"./public/img/thumbnails/ceven.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code","design"],
+		"tags":["meteor","ratchet","news"], // not used yet
+		"builtWith":["meteor.js","ratchet"],
+		"role":["design","full stack","concept",],
+		"press":[{"publication":"Hearst","link":"https://drive.google.com/file/d/0BwoR-O4bO_4hWWx5RFN3bHlPSnpBRTZCRk53YklkNmkxeF93/view?usp=sharing"}],
+		"link":{}
+	},
+	{
+		"title":"How to Ride a Dragon",
+		"slug":"dragon",
+		"description":"A 20 foot physical dragon is the controller for a dragon you are flying in a VR world. By using the reins and the movement on the physical dragon, you can navigate in realtime your virtual dragon through a magical and mythical world of floating islands.",
+		"thumbnail":"./public/img/thumbnails/dragon.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code","make"],
+		"tags":["unity","vr"], // not used yet
+		"builtWith":["unity 3D","arduino","node.js", "cinema4D"],
+		"role":["all code","3D modelling","animation","unity","fabrication","sensors","concept","experience"],
+		"press":[
+			{"publication":"Forbes","link":"http://www.forbes.com/sites/stevenrosenbaum/2015/12/24/can-a-robot-find-you-love-nyus-itp-winter-show-says-it-can/#23af74fb39cd"},
+			{"publication":"NYC Media Lab","link":"https://medium.com/@nycmedialab/vr-demos-take-flight-in-miami-98b4521a41a3#.qcik9jdc1"},
+			{"publication":"Techsite.io","link":"http://www.techsite.io/p/213957"}
+			],
+		"link":{}
+	},
+	{
+		"title":"Gif It To Me Now",
+		"slug":"gif-it-to-me-now",
+		"description":"Type out what you want your gif to be and this app creates a gif from that phrase. It searches images for each word you enter and combines them into your own unique gif overlayed with your text",
+		"thumbnail":"./public/img/thumbnails/gif-it-to-me-now.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code"],
+		"tags":["web","javascript","node"], // not used yet
+		"builtWith":["node.js","javascript"],
+		"role":["full stack","design","concept"],
+		"press":[],
+		"link":{}
+	},
+	{
+		"title":"Grep",
+		"slug":"grep",
+		"description":"Grep makes code search instantaneous, simple and smart. The app learns as users select correct answers to questions they enter improving its own accuracy and delivering results to questions faster. Built with ",
+		"thumbnail":"./public/img/thumbnails/grep.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code"],
+		"tags":["node","javascript","elasticsearch"], // not used yet
+		"builtWith":["elasticsearch","node.js","javascript","python"],
+		"role":["full stack","algorithms"],
+		"press":[{"publication":"","link":""}],
+		"link":{"display":"","link":"http://","status":"active"}
+	},
+	{
+		"title":"Hydrate.me",
+		"slug":"hydrate-me",
+		"description":"",
+		"thumbnail":"./public/img/thumbnails/hydrate-me.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["make"],
+		"tags":[""], // not used yet
+		"builtWith":["arduino","processing"],
+		"role":["fabrication","coding","circuit","concept","product design"],
+		"press":[{"publication":"Green House Stories","link":"http://www.greenhousestories.com/2014/12/09/prototyping-showcase/"}],
+		"link":{}
+	},
+	{
+		"title":"Imly",
+		"slug":"imly",
+		"description":"Imly was the first and largest homefood marketplace in India. Users could see in realtime what was cooking in people's homes around them and have unique home cooked dishes delivered to their doorstep. It spawned an entire industry and several other startups in the space.",
+		"thumbnail":"./public/img/thumbnails/imly.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["code"],
+		"tags":[""], // not used yet
+		"builtWith":["python","django"],
+		"role":["founder","design","UX","full stack","business"],
+		"press":[
+			{"publication":"Techcrunch","link":"https://techcrunch.com/2014/02/11/microsoft-accelerator-launches-new-program-for-late-stage-startups-in-india/"},
+			{"publication":"Your Story","link":"https://yourstory.com/2013/07/so-you-think-your-moms-cooking-is-outta-this-world-then-introduce-her-to-the-rest-of-the-world-through-imly-in/"},
+			{"publication":"Hindustan Times","link":"http://www.hindustantimes.com/business/microsoft-playing-mother-hen-to-dozens-of-indian-startups/story-QNkYMQBO6o3Cb760Xt1zHP.html"},
+			{"publication":"Next Big What","link":"https://news.nextbigwhat.com/home-cooked-food-in-mumbai-imly-297/"},
+			{"publication":"Forbes","link":"http://forbesindia.com/printcontent/39113"},
+			{"publication":"DNA India","link":"http://www.dnaindia.com/academy/report-entrepreneurs-equal-to-job-creators-2036167"},
+			{"publication":"VC Circle","link":"http://techcircle.vccircle.com/2014/02/07/mumbai-based-imly-in-wants-to-tap-into-the-flavour-of-homemade-food-connects-home-chefs-with-curious-foodies/"},
+			{"publication":"Microsoft","link":"https://news.microsoft.com/en-in/microsoft-ventures-in-india-announces-its-accelerator-programs-2014-summer-batch/"},
+			{"publication":"ZDNet","link":"http://www.zdnet.com/article/as-one-indian-accelerator-dies-another-one-spreads-its-wings/"},
+			{"publication":"Tech in Asia","link":"https://www.techinasia.com/microsoft-ventures-india-summer-2014-includes-later-stage-startups"},
+			{"publication":"Inc42","link":"https://inc42.com/buzz/microsoft-ventures-announces-16-startups-summer-acceleration-program/"},
+			{"publication":"First Post","link":"http://tech.firstpost.com/news-analysis/imly-an-online-marketplace-for-home-chefs-108829.html"},
+			{"publication":"Gadgets Now","link":"http://www.gadgetsnow.com/tech-news/How-Imly-crowdsources-home-cooked-food/articleshow/35072197.cms"},
+			{"publication":"India Today","link":"http://indiatoday.intoday.in/story/the-online-dabbawalas-web-kitchen-comfort-food-entrepreneurs-amateur-chefs/1/291022.html"},
+			{"publication":"Campus Diaries","link":"https://campusdiaries.com/stories/imly.in-is-your-answer-to-homemade-food"},
+			{"publication":"The Tribune","link":"http://www.tribuneindia.com/2014/20140503/trends.htm"},
+			],
+		"link":{"display":"imly.in","link":"http://imly.in","status":"inactive"}
+	},
+	{
+		"title":"Lumens",
+		"slug":"lumens",
+		"description":"Lumens are smart shoes whose look you can control and customize via its app. They also provide notifications from your phone to your feet and track your level of inactivity. You can even get your twitter/facebook notifications via custom animations and vibrations in the sole.",
+		"thumbnail":"./public/img/thumbnails/lumens.gif",
+		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
+		"category":["make"],
+		"tags":[""], // not used yet
+		"builtWith":["arduino","bluetooth","javascript","node","silicon","makerbot"],
+		"role":["code","sensors","circuit design","fabrication","app development", "product design", "concept"],
+		"press":[
+			{"publication":"NY1","link":"http://www.ny1.com/nyc/all-boroughs/tech-talk/2015/10/1/wearable-tech-innovations-on-display-at-nyc-media-lab-2015-annual-summit.html?cid=twitter_NY1"},
+			{"publication":"Make Zine","link":"http://makezine.com/2015/05/27/sweat-hugs-vr-ar-itp-spring-show/"}
+			],
+		"link":{"display":"Lumenshoe.com","link":"http://lumenshoe.com","status":"inactive"}
+	},
+	{
+		"title":"Outrage Machine(Times Square)",
+		"slug":"outrage-machine",
+		"description":"Aimed at interrogating social outrage through engaging screen based interactive storytelling experiences, onlookers could text in 'what outraged them' and see their name and outrage appear on the large Viacom screens in Times Square.",
+		"thumbnail":"./public/img/thumbnails/outrage-machine.gif",
+		"media":[{"path":"https://player.vimeo.com/video/154378147","caption":"","type":"video"}], //include embed link without iframe stufffor videos
+		"category":["code","play"],
+		"tags":[""], // not used yet
+		"builtWith":["node","javascript","photoshop"],
+		"role":["full stack","concept","design"],
+		"press":[{"publication":"Tisch NYU","link":"https://tisch.nyu.edu/itp/events/spring-2016/outrage-machine"}],
+		"link":{}
+	},
+	{
+		"title":"Peeqo",
+		"slug":"peeqo",
+		"description":"Peeqo is a personal desktop robotic assistant who expresses himself through GIFs.Designed for people who spend long hours at their desks, this pint sized robot responds to your voice to help with essential work tasks and provides little moments of delight and entertainment often needed to get through the day.",
+		"thumbnail":"./public/img/thumbnails/peeqo.gif",
+		"media":[{"path":"https://player.vimeo.com/video/166727389","caption":"","type":"video"}], //include embed link without iframe stufffor videos
+		"category":["make"],
+		"tags":[""], // not used yet
+		"builtWith":["raspberry pi","arduinos","fusion 360","3D printers","electron","node","javascript","python"],
+		"role":["3D modelling","3D printing","full stack","sensors","circuit design","fabrication","design","concept","animation","character design"],
+		"press":[
+			{"publication":"NY Post","link":"https://www.facebook.com/NYPost/videos/vb.134486075205/10157661502345206/?type=2&theater"}
+		],
+		"link":{"display":"Peeqo.com","link":"http://peeqo.com","status":"active"}
+	},
+	
+
+
 
 ]
 
@@ -30,10 +269,11 @@ var projectPlaceholder = {
 		"title":"",
 		"slug":"",
 		"description":"",
-		"thumbnail":"",
+		"thumbnail":"./public/img/thumbnails/",
 		"media":[{"path":"","caption":"","type":"image/video"}], //include embed link without iframe stufffor videos
 		"category":[""],
 		"tags":[""], // not used yet
+		"builtWith":[""],
 		"role":[""],
 		"press":[{"publication":"","link":""}],
 		"link":{"display":"","link":"http://","status":"active"}
